a4056dc8d2ae619a3436170861a4709e7d9fcf84
Junim <edsonjunior.1219@gmail.com>
2021-02-19 11:02:15 -0300

    Código 2021

---
+++ src/component/Button/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+const css = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttonContainer: {
+        width: '90%',
+        height: 50,
+        backgroundColor: '#17181a',
+        borderRadius: 5,
+        marginVertical: '5%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    textButtom: {
+        fontSize: 17,
+        color: '#fff'
+    }
+});
+
+export {css};
